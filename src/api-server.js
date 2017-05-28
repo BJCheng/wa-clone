6a2b7f694b1a0bbad930f857b8fbb4ed964d1f28
BJCheng <BJCheng@users.noreply.github.com>
2017-05-29 00:25:57 +0800

    generate redis REDISCLOUD_URL in this app

--- src/api-server.js
+++ src/api-server.js
@@ -1,12 +1,25 @@
 import express from 'express';
+import {setupRedis, redisClient} from './redis-client';
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+setupRedis();
 
 app.get('/', (req, res) => {
 	res.send('Hello React Class, from Express.js!');
 });
 
+app.get('/redis-test', (req, res)=>{
+	redisClient.incr('inc-test1', (err, result)=>{
+		if(err){
+			console.error(err);
+			res.send(err);
+		} else{
+			res.send(`New incremented value is : ${result}`);
+		}
+	});
+});
 
 app.listen(port, () => {
 	console.log(`Express app listening on port ${port}`);
